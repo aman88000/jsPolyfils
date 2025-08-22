5c752b483a697d1f050942049289040cbdcdb1ee
aman88000 <aman88000@gmail.com>
2025-08-23 00:10:44 +0530

    polyfil of object.create which can set prototype of another object

---
+++ ObjectCreatepolyfil.js
@@ -0,0 +1,16 @@
+
+const obj1 = {
+    name: 'aman',
+    getName(){
+        return this.name
+    }
+}
+
+function ObjectCreatePolyfil(fromObj){
+    const newObj = {}
+    Object.setPrototypeOf(newObj, fromObj)
+    return newObj
+}
+
+const obj2 = ObjectCreatePolyfil(obj1)
+console.log(obj2.name)
